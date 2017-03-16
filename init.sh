2aab6c55be2b02eef6a36725747186c51b96a2f2
Chris Stryczynski <chris@soundshare.co.uk>
2017-03-16 18:08:32 +0000

    Hardcode network names

--- init.sh
+++ init.sh
@@ -8,4 +8,7 @@ fi
 if [ ! -d "data/ArchPackageCompareStats" ] ; then
     git clone git@github.com:chrissound/ArchPackageCompareStats.git data/ArchPackageCompareStats
 fi
+
+sudo docker network prune
+sudo docker network create archpkgcompare_webserver_default
 sudo docker-compose up
